242bfd36d46e4476dea5d2af132af50ebcefe908
arunjos2000 <arunjoseph.14.aj@gmail.com>
2021-06-13 23:41:44 +0530

    updated

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function addTodo(e){
     //Append to List
     todoList.appendChild(todoDiv);
     //CLear Todo Input VAlue
-    todoInput.value="";
+    todoInput.value="s";
 }
 function deleteCheck(e){
     const item= e.target;
